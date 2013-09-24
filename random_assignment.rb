e927b569daf8b0cd65e0a1f15cb0bff76aab7cf0
Meeka <rrgayhart@gmail.com>
2013-09-24 12:45:28 -0600

    Creates random assignment code

---
+++ random_assignment.rb
@@ -0,0 +1,7 @@
+class_members = ["Romeeka","George","Luke",] #["Nikhil"],["Jonah"],["Ben H"], ["Persa"], ["Billy"] ["Bryana"], ["Adam"], ["Tyler"]]
+shuffle_class_members = class_members.shuffle
+classmate1 = shuffle_class_members[0]
+classmate2 = shuffle_class_members[1]
+
+puts "#{classmate1} will come up with the assignment"
+puts "#{classmate2} will complete the assignment"
