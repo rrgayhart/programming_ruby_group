276f4951a83a076ab3e157dcbdbd3573a570e348
Meeka <rrgayhart@gmail.com>
2013-09-24 13:57:40 -0600

    Adds hashes of parts and chapters to random_assignment

--- random_assignment.rb
+++ random_assignment.rb
@@ -5,3 +5,16 @@ classmate2 = shuffle_class_members[1]
 
 puts "#{classmate1} will come up with the assignment"
 puts "#{classmate2} will complete the assignment"
+
+parts = { 1 => [3..208], 2 => [209..296], 3 => [297..416] }
+
+chapters_part_one = { 1 => "Getting Started", 2 => "Ruby.new", 3 => "Classes, Objects, and Variables", 4 => "Containers, Blocks, and Iterators", 5 => "Sharing Functionality: Inheritance, Modules, and Mixins", 6 => "Standard Types", 7 => "Regular Expressions", 8 => "More About Methods", 9 => "Expressions", 10 => "Exceptions, Catch, and Throw", 11 => "Basic Input and Output", 12 => "Fibers, Threads, and Processes", 13 => "Unit Testing", 14 => "When Trouble Strikes!" }
+
+chapters_part_two = { 15 => "Ruby and Its World", 16 => "Namespaces, Source Files, and Distribution", 17 => "Character Encoding", 18 => "Interactive Ruby Shell", 19 => "Documenting Ruby", 20 => "Ruby and the Web", 21 => "Ruby and Microsoft Windows" }
+
+chapters_part_three = { 22 => "The Ruby Language", 23 => "Duck Typing", 24 => "Metaprogramming", 25 => "Reflection, ObjectSpace, and Distributed Ruby", 26 => "Locking Ruby in the Safe" }
+
+all_chapters = chapters_part_one.merge(chapters_part_two.merge(chapters_part_three))
+
+
+
